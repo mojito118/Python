ca3b09d4ddc887745cfd274a98fd1d49acc91dd3
mojito118 <kmk762@hanmail.net>
2018-12-09 17:48:35 +0900

    [Test] Get page HTML and parse

--- login.py
+++ login.py
@@ -1,2 +1,22 @@
-import os
-print("Hello")
+from selenium import webdriver as wb
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+URL = "https://torrentwal.net/torrent_variety/torrent1.htm"
+response = requests.get(URL)
+
+print(response.status_code)
+htmlContent = response.content
+
+text = bs(htmlContent, 'html.parser')
+
+print(text.prettify())
+
+
+#browser = wb.Edge()
+#browser.get("https://torrentwal.net/torrent_variety/torrent1.htm")
+#var = browser.
+#print(var)
+
+
